2164e5de089bb0871f74ce0f6a52872255f31484
Khush Narang <106396575+KhushNarang1@users.noreply.github.com>
2023-12-26 21:24:33 +0530

    changes

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Profile
+
+class profileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [profileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+# admin.site.register(Profile)
 
 # Register your models here.
--- dwitter/views.py
+++ dwitter/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Profile
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+    return render(request, "dwitter/profile.html", {"profile" : profile})
+
+def dashboard(request):
+    return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user = request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles" : profiles})
 
 # Create your views here.
