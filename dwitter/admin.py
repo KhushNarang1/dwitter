cdfcc082e54defd6b1e3d476388ed53b21e0bb36
Khush Narang <106396575+KhushNarang1@users.noreply.github.com>
2023-12-27 12:06:05 +0530

    third part

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Profile
+from .models import Profile, Dweet
+
 
 class profileInline(admin.StackedInline):
     model = Profile
@@ -14,6 +15,7 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Dweet)
 # admin.site.register(Profile)
 
 # Register your models here.
